5fe13566a0b71612ce88fc2980960576906e00d7
haerxeong <haeryeong04@naver.com>
2024-10-31 01:56:16 +0900

    데이터 추가

--- app/src/main/java/com/example/bookie/UnreadBookActivity.kt
+++ app/src/main/java/com/example/bookie/UnreadBookActivity.kt
@@ -8,7 +8,10 @@ import com.example.bookie.databinding.ActivityUnreadBookBinding
 class UnreadBookActivity: AppCompatActivity() {
     val unreadbooks = arrayOf(
         MyBook("불편한 편의점", "김호연", "나무옆의자", 2021),
-        MyBook("불편한 편의점2", "김호연", "나무옆의자", 2021)
+        MyBook("호밀밭의 파수꾼", "제롬 데이비드 샐린저", "민음사", 2023),
+        MyBook("채식주의자", "한강", "창비", 2022),
+        MyBook("코스모스", "칼 세이건", "사이언스북스", 2006),
+        MyBook("물고기는 존재하지 않는다", "룰루 밀러", "곰출판", 2021)
     )
 
     lateinit var binding: ActivityUnreadBookBinding
