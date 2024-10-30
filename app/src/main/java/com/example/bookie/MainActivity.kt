dad6a7ad0a68e0c3fdc204fffd856f2026a7fe38
hahyunjung <106101166+hahyunjung@users.noreply.github.com>
2024-10-31 02:03:52 +0900

    Page : 독서일기 작성 페이지 1

--- app/src/main/java/com/example/bookie/MainActivity.kt
+++ app/src/main/java/com/example/bookie/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.bookie
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.bookie.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Use View Binding to access the button
+        binding.publishButton.setOnClickListener {
+            Toast.makeText(this, "독서일기가 발행되었습니다!", Toast.LENGTH_SHORT).show()
         }
     }
 }
