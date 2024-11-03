d728dd807719a00a7b28a0ec44fce54057b0877d
KimJinju <pmsuk4023@naver.com>
2024-11-03 11:07:34 +0900

    독서 예정 책 추가 프레그먼트 layout만 만들어논 것

---
+++ app/src/main/java/com/example/bookie/AddUnreadBookFragment.kt
@@ -0,0 +1,42 @@
+package com.example.bookie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AddUnreadBookFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+         */
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_unread_book, container, false)
+    }
+/*
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AddUnreadBookFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+ */
+}
